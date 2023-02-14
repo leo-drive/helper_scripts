c8749cd104de30c4ed19a0b7e8ca653439d3ae5c
Nilay <106528015+NilaySener@users.noreply.github.com>
2023-02-14 13:35:03 +0300

    Update force_ip.sh

--- camera_ip_config/force_ip.sh
+++ camera_ip_config/force_ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MAC1=1C0FAF2E667F # MAC address of camera1 
-MAC2=1C0FAF2A7C10# MAC address of camera2
+MAC2=1C0FAF2A7C10 # MAC address of camera2
 MAC3=1C0FAF2E8016 # MAC address of camera3 
 
 TOP_CAM_IP=169.207.128.102 # IP address of camera1 
